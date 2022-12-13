2e0ebb0efe293884a958925debab0be18d8b7d60
ssunny0208 <xdfv635@snu.ac.kr>
2022-12-13 16:48:32 +0900

    221213

--- matchGame.js
+++ matchGame.js
@@ -1021,6 +1021,7 @@ function Window(n) {
     fill(87, 68, 111);
   }
   rectMode(CORNER);
+  pop();
   rect(windowX + 2, windowY, windowW - 2, windowH);
 
   for (let i = 0; i < SNOWFLAKE_COUNT; i++) {
